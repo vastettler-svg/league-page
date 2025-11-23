d7112f60825eb3c4192bc9978b2a7d537d914b90
vastettler-svg <vastettler@gmail.com>
2025-11-22 23:11:57 -0700

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,6 +1,6 @@
 /*   STEP 1   */
 export const leagueID = "1188650510165381120"; // your league ID
-export const leagueName = "NFL Tier - NFC"; // your league name
+export const leagueName = "MFL - NFL Tier (NFC)"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
